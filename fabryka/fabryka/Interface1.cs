61de155ca447f25df8e839ab79fdd277c286a027
NataliaK97 <NataliaK97@users.noreply.github.com>
2018-03-22 11:14:39 +0100

    interface i klasa products

---
+++ fabryka/fabryka/Interface1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fabryka
+{
+    public interface Interface1
+    {
+        String listItems();
+        String addItem();
+        String deleteItem();
+    }
+}
---
+++ fabryka/fabryka/products.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fabryka
+{
+    public class produsts : Interface1
+    {
+        public String listItems()
+        {
+            return "Lista produktów";
+        }
+        public String addItem()
+        {
+            return "Dodaje produkt";
+        }
+        public String deleteItem()
+        {
+            return "Usuwa produkt";
+        }
+    }
+}

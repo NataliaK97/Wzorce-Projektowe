3271fe2448bc60d0b612e20dd47d0f05ef953971
NataliaK97 <NataliaK97@users.noreply.github.com>
2018-03-22 15:11:39 +0100

    klasa users

---
+++ fabryka/fabryka/users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fabryka
+{
+    public class users : Interface1
+    {
+        public String listItems()
+        {
+            return "Lista uzytkowników";
+        }
+        public String addItem()
+        {
+            return "Dodaje uzytkownika";
+        }
+        public String deleteItem()
+        {
+            return "Usuwa uzytkownika";
+        }
+    }
+}

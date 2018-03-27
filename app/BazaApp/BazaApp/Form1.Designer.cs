0e748458b42cefae728de87e4eab4bc066d43dc4
NataliaK97 <NataliaK97@users.noreply.github.com>
2018-03-27 13:29:46 +0200

    plik do tworzenia bazy

--- app/BazaApp/BazaApp/Form1.Designer.cs
+++ app/BazaApp/BazaApp/Form1.Designer.cs
@@ -143,8 +143,10 @@
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(312, 61);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(345, 377);
             this.dataGridView1.TabIndex = 14;
             // 

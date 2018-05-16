21f3c48adf229e256326aa2b93a1a83d1d31f08d
NataliaK97 <NataliaK97@users.noreply.github.com>
2018-05-16 17:58:49 +0200

    stworzenie klasy player i enum position

---
+++ ChainOfResponsibility/ChainOfResponsibility/ChainOfResponsibility/Players.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    abstract class Player
+    {
+        public string Name { get; set; }
+        protected Player nextPlayer;
+
+        public void SetNextPlater(Player player)
+        {
+            nextPlayer = player;
+        }
+
+        public abstract void playersPosition(Position position); 
+    }
+}

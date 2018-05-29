01e1338bd33bf102f4b016f3518b70fe4954ef2a
NataliaK97 <NataliaK97@users.noreply.github.com>
2018-05-29 17:11:19 +0200

    pozostałe klasy

---
+++ Proxy/Proxy/DaneUżytkownika.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class DaneUżytkownika :Interface
+    {
+        Interface Ocena;
+        private string haslo;
+        public bool hasloOk;
+
+        public void getHaslo (string _haslo)
+        {
+            haslo = _haslo;
+            SprawdzHaslo();
+        }
+        private void SprawdzHaslo()
+        {
+            if (haslo=="hasloOkon")
+            {
+                otworzDostep();
+                hasloOk = true;
+            }
+            else
+            {
+                hasloOk = false;
+            }
+        }
+        private void otworzDostep()
+        {
+            OcenaFactory ocenaFactory = new OcenaZDziennka();
+            Ocena = ocenaFactory.Dziennik();
+        }
+        public double WyswietlOcene()
+        {
+            return Ocena.WyswietlOcene();
+        }
+        public void ZmienOcene(double nowaOcena)
+        {
+            Ocena.ZmienOcene(nowaOcena);
+        }
+    }
+}

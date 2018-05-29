ea64501e3dbfcd3566aa9dfc955bc82ce513ccd8
NataliaK97 <NataliaK97@users.noreply.github.com>
2018-05-29 17:10:46 +0200

    interface i klasa ocena

---
+++ Proxy/Proxy/Interface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    interface Interface
+    {
+        double WyswietlOcene();
+        void ZmienOcene(double nowaOcena);
+    }
+}
---
+++ Proxy/Proxy/Ocena.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Ocena : Interface
+    {
+        private double ocena = 5.0;
+        public double WyswietlOcene()
+        {
+            return ocena;
+        }
+        public void ZmienOcene(double nowaOcena)
+        {
+            ocena = nowaOcena;
+        }
+    }
+}

b2612ced1a3a476b2c4759bce40a8c5228a5ae0a
NataliaK97 <NataliaK97@users.noreply.github.com>
2018-06-04 13:26:42 +0200

    klasy

---
+++ Composite/Composite/Gold.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Gold : Interface
+    {
+        public void stworz(string kolor)
+        {
+            Console.WriteLine("Kolczyki złote, dodatki w kolorze: "+kolor);
+        }
+    }
+}
---
+++ Composite/Composite/Silver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public class Silver : Interface
+    {
+        public void stworz(string kolor)
+        {
+            Console.WriteLine("Kolczyki srebrne, dodatki w kolorze: "+kolor);
+        }
+    }
+}

5f2c7eec9bf9664bc931f1d8b34e90263df9b460
NataliaK97 <NataliaK97@users.noreply.github.com>
2018-06-04 13:41:20 +0200

    gotowy composite

---
+++ Composite/Composite/Makeing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Makeing : Interface
+    {
+        private List<Interface> lista = new List<Interface>();
+
+        public void stworz(string kolor)
+        {
+            foreach (Interface inter in lista)
+            {
+                inter.stworz(kolor);
+            }
+        }
+
+        public void add(Interface i)
+        {
+            lista.Add(i);
+        }
+        public void remove(Interface i)
+        {
+            lista.Remove(i);
+        }
+    }
+}
--- Composite/Composite/Program.cs
+++ Composite/Composite/Program.cs
@@ -10,6 +10,22 @@ namespace Composite
     {
         static void Main(string[] args)
         {
+            Interface kolczyki01 = new Silver();
+            Interface kolczyki02 = new Gold();
+            Interface kolczyki03 = new Wood();
+
+            Makeing makeing = new Makeing();
+
+            makeing.add(kolczyki01);
+            makeing.stworz("Czerwony");
+
+            makeing.add(kolczyki02);
+            makeing.stworz("Zielony");
+            
+            makeing.add(kolczyki03);
+            makeing.stworz("Niebieski");
+
+            Console.ReadKey();
         }
     }
 }
